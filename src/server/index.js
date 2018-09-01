6821f918b94fb275c56451e940a6cc26e9326af2
Roman Kalita <raman_kalita@mail.ru>
2018-09-01 14:36:37 +0300

    part-1: Add simple ReactJS app

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,13 @@
 import express from 'express';
+import { resolve, join } from 'path';
 const app = express()
 
 const port = process.env.PORT || 8000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use('/bundle', express.static(resolve(__dirname, '../client/bundle')));
+
+app.get('/', (req, res) => res.sendFile('index.html', {
+    root: resolve(__dirname, '../client/')
+}))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))

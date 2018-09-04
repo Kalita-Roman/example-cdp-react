5874a097a948edf5d9946a98cdec6f8b0b6d0b0e
Roman Kalita <raman_kalita@mail.ru>
2018-09-04 15:17:04 +0300

    part-1: Add some React components

--- src/webpack/webpack.config.js
+++ src/webpack/webpack.config.js
@@ -19,6 +19,9 @@ export default {
         new webpack.optimize.OccurrenceOrderPlugin(),
         new webpack.HotModuleReplacementPlugin(),
     ],
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
 
     module: {
         rules: [
